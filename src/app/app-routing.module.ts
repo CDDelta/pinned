6a93ce9d6f7a6e04ecdc3dedb5e8cd080acb37dc
CDDelta <ngjunyen@gmail.com>
2020-07-04 22:12:06 +1000

    scaffold app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () =>
+      import('./landing/landing-routing.module').then(
+        (m) => m.LandingRoutingModule,
+      ),
+  },
+  {
+    path: 'p',
+    loadChildren: () =>
+      import('./profile/profile-routing.module').then(
+        (m) => m.ProfileRoutingModule,
+      ),
+  },
+  {
+    path: 'upload',
+    loadChildren: () =>
+      import('./upload/upload-routing.module').then(
+        (m) => m.UploadRoutingModule,
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
