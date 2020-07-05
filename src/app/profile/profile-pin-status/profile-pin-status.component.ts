58dfa4857b419de15c164a0e72bbe9c6e359d815
CDDelta <ngjunyen@gmail.com>
2020-07-05 10:50:49 +1000

    add pin status page

--- src/app/profile/profile-pin-status/profile-pin-status.component.ts
+++ src/app/profile/profile-pin-status/profile-pin-status.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, timer, combineLatest } from 'rxjs';
+import { map, flatMap, tap } from 'rxjs/operators';
+import { PinService } from '../../shared/services/pin.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile-pin-status',
   templateUrl: './profile-pin-status.component.html',
-  styleUrls: ['./profile-pin-status.component.scss']
+  styleUrls: ['./profile-pin-status.component.scss'],
 })
 export class ProfilePinStatusComponent implements OnInit {
+  public profileId: string;
+  public pinId: string;
+  public pinPublished$: Observable<boolean>;
 
-  constructor() { }
+  constructor(private pinService: PinService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.pinPublished$ = combineLatest(
+      this.route.params.pipe(
+        tap((params) => (this.profileId = params['profileId'])),
+        map((params) => {
+          this.pinId = params['pinId'];
+          return this.pinId;
+        }),
+      ),
+      timer(0, 30 * 1000),
+    ).pipe(flatMap(([pinId]) => this.pinService.getPinPublishStatus(pinId)));
   }
-
 }
