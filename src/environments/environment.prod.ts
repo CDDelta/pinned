139775a24fe1878172da0f8cd37ae36a62b87390
CDDelta <ngjunyen@gmail.com>
2020-07-05 16:36:05 +1000

    prepare for release

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  appName: 'pinned',
+  appVersion: '0.9.1',
+  arweavePeerDomain:
+    'https://tzzm25u6u64tn7eyt6vdubwucmp32sa3ifoqop4n5dmds3gycjbq.arweave.net',
 };
